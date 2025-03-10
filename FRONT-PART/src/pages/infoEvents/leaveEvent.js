f4a238bd8530e883f9c4bb4d0c27ff0f7b87b00c
PinchoPel <noxam13@gmail.com>
2025-03-10 10:36:51 +0100

    guardar

--- FRONT-PART/src/pages/infoEvents/leaveEvent.js
+++ FRONT-PART/src/pages/infoEvents/leaveEvent.js
@@ -7,18 +7,28 @@ import { getUserName } from "./getUserName.js";
 export const leaveEvent = async  (sectionEvents, eventDivEnlarged, author) => {
     const myEvents = document.querySelector("#Mis-eventos");
     const userName = getUserName();
-    if (myEvents.textContent == "Volver al Inicio" && userName == author) {
-        await renderMyCreatedEvents();     
-        }
-    else if (myEvents.textContent == "Volver al Inicio" && userName !== author){
-        await renderSignedUpEvent();
-    }
-    else{
+    if (!myEvents) {
         const aside = document.querySelector("#asideSectionHome");
         aside.classList.remove("hidden");
         sectionEvents.classList.remove("singleEvent");
         eventDivEnlarged.classList.remove("eventDivEnlarged");
         const events = await eventsFetch("orderdates/1");
         await renderEvents(events);
+    }
+    else if (myEvents) {
+        if (myEvents.textContent == "Volver al Inicio" && userName == author) {
+            await renderMyCreatedEvents();     
+            }
+        else if (myEvents.textContent == "Volver al Inicio" && userName !== author){
+            await renderSignedUpEvent();
+        }
+        else{
+            const aside = document.querySelector("#asideSectionHome");
+            aside.classList.remove("hidden");
+            sectionEvents.classList.remove("singleEvent");
+            eventDivEnlarged.classList.remove("eventDivEnlarged");
+            const events = await eventsFetch("orderdates/1");
+            await renderEvents(events);
+        }
     }
 };
