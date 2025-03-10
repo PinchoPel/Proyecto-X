206d893d5849b364276d0a3874460dc889790517
PinchoPel <noxam13@gmail.com>
2025-03-10 12:47:45 +0100

    guardar

--- BACK-PART/index.js
+++ BACK-PART/index.js
@@ -21,6 +21,7 @@ app.use("*", (req,res,next) => {
     return res.status(404).json("Ruta no encontrada")
 });
 
-app.listen(3000, () => {
-    console.log("Servidor conectado en: http://localhost:3000/");   
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+    console.log(`Servidor conectado en: http://localhost:${port}/`);   
 });
