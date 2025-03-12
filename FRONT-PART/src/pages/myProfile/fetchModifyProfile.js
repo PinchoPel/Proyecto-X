c5f2a92f4f264575bbaf66639195c8d9e86af3fa
PinchoPel <noxam13@gmail.com>
2025-03-12 12:40:07 +0100

    guardar

--- FRONT-PART/src/pages/myProfile/fetchModifyProfile.js
+++ FRONT-PART/src/pages/myProfile/fetchModifyProfile.js
@@ -22,28 +22,28 @@ export const fetchModifyProfile = async (route, formSelector) => {
             renderMyProfile(await fetchUser(`myProfile/${userId.textContent}`));
         }
        else if (!response.ok) {
-            const errorMessage = await response.json();
-            let arrayErrors = errorMessage.errors;
-            const divDataProfile = document.querySelector("#divDataProfile");
-    
-            const createPError = async (ownClass, textContent) => {
-                const errorFormMessage = document.createElement("p");
-                errorFormMessage.classList.add(ownClass, "errorFormMessage");
-                errorFormMessage.textContent = textContent;
-                divDataProfile.append(errorFormMessage);
-            };
-            arrayErrors.forEach(error => {
-                if (error === `"userName" length must be at least 3 characters long` && !document.querySelector(".errorModifProfileName")) {
-                    createPError("errorModifProfileName", "El nombre de usuario debe tener entre 3 y 10 caracteres")
-                }
-                if (error === `"email" must be a valid email` && !document.querySelector(".errorModifProfileEmail")) {
-                    createPError("errorModifProfileEmail", "Se debe introducir un correo electrónico válido")
-                }
-                if (error === `"password" length must be at least 5 characters long` && !document.querySelector(".errorModifProfilePass")) {
-                    createPError("errorModifProfilePass",  "La contraseña debe tener 5 caracteres como mínimo")
-                }
-            });
-        return
+        const errorMessage = await response.json();
+        let arrayErrors = errorMessage.errors;
+        const divDataProfile = document.querySelector("#divDataProfile");            
+        const createPError =  (ownClass, textContent) => {
+            console.log("punto0");
+            const errorFormMessage = document.createElement("p");
+            errorFormMessage.classList.add(ownClass, "errorFormMessage");
+            errorFormMessage.textContent = textContent;
+            divDataProfile.append(errorFormMessage);
+        };
+        arrayErrors.forEach(error => {
+            if (error.includes("userName") && !document.querySelector(".errorModifProfileName")) {
+                createPError("errorModifProfileName", "El nombre de usuario debe tener entre 3 y 10 caracteres");
+            }
+            if (error.includes("email") && !document.querySelector(".errorModifProfileEmail")) {
+                createPError("errorModifProfileEmail", "Se debe introducir un correo electrónico válido");
+            }
+            if (error.includes("password") && !document.querySelector(".errorModifProfilePass")) {
+                createPError("errorModifProfilePass", "La contraseña debe tener 5 caracteres como mínimo");
+            }
+        });
+    return;
         }
        } catch (error) {
                console.error("Error al cargar el perfil:", error);
