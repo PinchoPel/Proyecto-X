6d90bb835d0588a4e18081e829fc03f974b9b823
PinchoPel <noxam13@gmail.com>
2025-03-12 11:25:00 +0100

    guardar

--- FRONT-PART/src/pages/login-register/sectionLogin.js
+++ FRONT-PART/src/pages/login-register/sectionLogin.js
@@ -5,6 +5,7 @@ import { registerForm } from "./register.js";
 export const sectionLogin = () => {
     const main = document.querySelector("main");
     const navBar = document.querySelector("nav");
+    const footer = document.querySelector("footer");
     main.innerHTML = "";
     navBar.innerHTML = "";
     linkCSS("./src/pages/login-register/login.css");
@@ -16,7 +17,8 @@ export const sectionLogin = () => {
     buttonHome.id = "buttonHome";
     buttonHome.textContent = "Volver a Inicio";
     sectionLogin.id = "sectionLogin";
-    navBar.classList = "navBarLogin"
+    navBar.classList = "navBarLogin";
+    footer.classList ="termsFooter";
     buttonHome.addEventListener("click", () => {location.reload()});
 
     navBar.append(buttonHome);
